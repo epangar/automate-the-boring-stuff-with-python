c315f605dc37bc8cab4efcb21c6fb7871c69aa70
Estanislao Pan García <epan@indra.es>
2020-01-03 14:28:57 +0100

    Ending early with Sys

---
+++ 4-Ending-early-with-sys.py
@@ -0,0 +1,15 @@
+#This code imports sys and random, and exists when the number is 0
+
+import random
+import sys
+
+while True:
+    n = random.randint(0,9)
+    print n
+
+    if n!= 0:
+        print("Let\s keep going...")
+        continue
+    else:
+        print("0! Bye!")
+        sys.exit()
