5364b15baedb1a5b46b087343df5ccfa5f89dc95
Estanislao Pan García <epan@indra.es>
2020-01-03 15:22:49 +0100

    6 - guess the number

---
+++ 5-Try-andError.py
@@ -0,0 +1,14 @@
+import random
+
+def divide(divisor):
+    try:
+        return 100 / divisor
+    except ZeroDivisionError:
+        print("ERROR, DIVIDE BY ZERO")
+
+
+for i in range (0,20):
+    number = random.randint(0,5)
+    print number
+
+    print divide(number)
---
+++ 6-Guess-the-number.py
@@ -0,0 +1,28 @@
+import random, sys
+
+print('You have 3 chances to guess the number, between 1 and 10')
+
+current_guesses = 0;
+secret_number = random.randint(1, 10)
+
+print secret_number
+
+while current_guesses < 3:
+    user_input = int(raw_input( 'Guess the number: ' ))
+
+    if secret_number == user_input:
+        print('That is correct! Bye!')
+        sys.exit()
+    elif secret_number > user_input:
+        print('Bigger!')
+        current_guesses += 1
+        continue
+    elif secret_number < user_input:
+        print('Lower!')
+        current_guesses += 1
+        continue
+    
+
+if (current_guesses == 3):
+    print('You lose! :(')
+    sys.exit()
