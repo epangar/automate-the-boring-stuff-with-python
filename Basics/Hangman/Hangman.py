10fd9b689f06c012f8d662945b6ac54bfca8c22f
Estanislao Pan García <epan@indra.es>
2020-01-10 14:56:11 +0100

    Hangman

--- Basics/Hangman/Hangman.py
+++ Basics/Hangman/Hangman.py
@@ -1,6 +1,6 @@
 import Hangman_Words, sys, random
 
-total_tries = 3
+
 
 def choose_word():
     list = Hangman_Words.words
@@ -8,22 +8,58 @@ def choose_word():
     position = random.randint(0, le)
     return list[position]
 
+total_tries = 10
 secret_word = choose_word()
-user_total_input_so_far = "-" * len(secret_word)
+guessed_so_far = "-" * len(secret_word)
+
+def update_guessed_so_far(input):
+    global guessed_so_far
+    guessed_so_far = list(guessed_so_far)
+
+    for j in range(0, len(secret_word)):
+        # print (j, secret_word, secret_word[j], guessed_so_far, input)
+
+        if secret_word[j] == input:
+            guessed_so_far[j] = input
+
+    guessed_so_far = ''.join(guessed_so_far)
+
+
+
+def check(input):
+    
+    current_input = input.upper()
+    if current_input in secret_word:
+        print ("Yes, %s appears in the word! Keep going." % current_input)
+        update_guessed_so_far(current_input)
+    else:
+        print ("Nope!")
+        global total_tries 
+        total_tries -= 1
+        
+        if (total_tries > 1):
+            print ('You still have %d tries.' % total_tries)
+        elif (total_tries == 1):
+            print ('You still have 1 try.')
+    
+    global guessed_so_far
+    print (guessed_so_far)
 
 def ask():
+    
     user_input = input('Please add a character: ')
-
-def check():
+    check(user_input)
 
 
 def win():
     print ('You won! Congratulations! :D')
     sys.exit()
 
+
 def lose():
     print ('You lose! Sorry :(')
     sys.exit()
+
 
 print ('I have chosen a random word. You have 10 tries.')
 
@@ -31,10 +67,4 @@ while total_tries >= 1:
 
     ask()
 
-    if (total_tries > 1):
-        print ('You still have %d tries.' % total_tries)
-    elif (total_tries == 1):
-        print ('You still have 1 try.')
     
-    print (total_tries)
-    total_tries -= 1
