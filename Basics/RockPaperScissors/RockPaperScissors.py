e69e668693b302a07a796811dd696298af329f31
Estanislao Pan García <epan@indra.es>
2020-03-18 18:19:16 +0100

    Start building RockPaperScissors

---
+++ Basics/RockPaperScissors/RockPaperScissors.py
@@ -0,0 +1,17 @@
+import sys, random
+
+def ask_how_many_turns():
+    turns = input('How many turns do you want to play?\n>>>')
+    
+    return turns
+
+
+def round():
+    
+
+def game():
+    rounds = ask_how_many_turns()
+
+    print (rounds)
+
+game()
