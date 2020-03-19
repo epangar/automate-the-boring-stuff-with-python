b80b434a3e56bbb69831e32c89676cc8adfab7d7
Estanislao Pan García <epan@indra.es>
2020-03-19 16:05:38 +0100

    RPS with a bug

--- Basics/RockPaperScissors/RockPaperScissors.py
+++ Basics/RockPaperScissors/RockPaperScissors.py
@@ -12,13 +12,15 @@ def ask_how_many_turns():
 
 def computer_choice():
     print("The computer has made its choice.")
-    return choices[random.randint(0,3)]
+    return choices[random.randint(0,2)]
 
 def my_choice():
     choice = int(input('Whats your choice?\n 0) Rock\n 1) Paper\n 2) Scissors\n>>> '))
 
+    if choice not in range(0,2):
+        print('I\'m sorry, I don\'t understand')
     
-    if  choice < 0 or choice > 2:
+    elif  choice < 0 or choice > 2:
         print('I\'m sorry, I don\'t understand.\n')
         my_choice()
     else:
@@ -26,11 +28,11 @@ def my_choice():
         return choices[choice]
 
 def decide(computer, player):
-    print("The computer chose %s, you chose %s." % computer, player)
+    print("The computer chose %s, you chose %s." % (computer, player))
 
     if computer == player:
         print("It\'s a tie!")
-        return
+        return "tie"
     elif (computer == "Rock" and player == "Scissors") or (computer == "Paper" and player == "Rock") or (computer == "Scissors" and player == "Paper"):
         print("The computer wins this round!")
         return "computer"
@@ -48,6 +50,15 @@ def play_round(n):
     winner = decide(adversary_choice, player_choice)
     return winner
 
+def end_game(score):
+    if score['player'] == score['computer']:
+        print( 'It\'s a tie! Better luck next time.')
+    elif score['player'] > score['computer']:
+        print('You won! Congratulations!')
+    else:
+        print('The computer won! Better luck next time.') 
+    sys.exit()
+
 def game():
 
     rounds = ask_how_many_turns()
@@ -55,9 +66,11 @@ def game():
 
     for i in range (0, rounds):
         key = str(play_round(i+1))
-        print(key)
-        score[key] += 1
-        print (score)
-
+        if key == "tie":
+            pass
+        else:
+            score[key] += 1
+        print ('[ %r - %r ]\n' % (score['computer'], score['player']))
+    end_game(score)
 
 game()
